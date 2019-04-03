bdc1449d299921a95cd3567bcc3a40a4734df2a6
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2019-04-03 14:27:55 +0200

    Move network logic to AuxBrain object

--- src/main/kotlin/nl/pindab0ter/eggbot/commands/ContractIDs.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/commands/ContractIDs.kt
@@ -1,13 +1,10 @@
 package nl.pindab0ter.eggbot.commands
 
-import com.auxbrain.ei.EggInc
-import com.github.kittinunf.fuel.httpGet
 import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
 import nl.pindab0ter.eggbot.daysHoursAndMinutes
 import nl.pindab0ter.eggbot.formattedName
-import nl.pindab0ter.eggbot.network.GET_CONTRACTS_URL
-import nl.pindab0ter.eggbot.network.base64Decoded
+import nl.pindab0ter.eggbot.network.AuxBrain
 import nl.pindab0ter.eggbot.toDateTime
 import org.joda.time.DateTime
 import org.joda.time.Duration
@@ -22,16 +19,12 @@ object ContractIDs : Command() {
         guildOnly = false
     }
 
-
-    // TODO: Refactor and handle failure
     override fun execute(event: CommandEvent) {
-        val contracts = EggInc.GetContractsResponse
-            .parseFrom(GET_CONTRACTS_URL.httpGet().response().second.body().base64Decoded())
-            .contractsList
-            .sortedBy { it.expirationTime }
+        AuxBrain.getContracts { getContractsResponse ->
+            val contracts = getContractsResponse.contractsList
+                .sortedBy { it.expirationTime }
 
-        when {
-            contracts.isNotEmpty() -> event.reply(StringBuilder(ACTIVE_CONTRACTS).appendln().apply {
+            if (contracts.isNotEmpty()) event.reply(StringBuilder(ACTIVE_CONTRACTS).appendln().apply {
                 contracts.forEach { contract ->
                     append("**`${contract.identifier}`**: ")
                     append("${contract.name} (${contract.egg.formattedName})")
@@ -47,7 +40,8 @@ object ContractIDs : Command() {
                     appendln()
                 }
             }.toString())
-            else -> event.replyWarning(ContractIDs.NO_ACTIVE_CONTRACTS)
+            else event.replyWarning(ContractIDs.NO_ACTIVE_CONTRACTS)
+
         }
     }
 }
--- src/main/kotlin/nl/pindab0ter/eggbot/network/AuxBrain.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/network/AuxBrain.kt
@@ -1,7 +1,19 @@
 package nl.pindab0ter.eggbot.network
 
-const val GET_SALES_URL = "http://www.auxbrain.com/ei/get_sales"
-const val GET_EVENTS_URL = "http://www.auxbrain.com/ei/get_events"
-const val GET_CONTRACTS_URL = "http://www.auxbrain.com/ei/get_contracts"
-const val FIRST_CONTACT_URL = "http://www.auxbrain.com/ei/first_contact"
-const val DAILY_GIFT_URL = "http://www.auxbrain.com/ei/daily_gift_info"
+import com.auxbrain.ei.EggInc
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.fuel.util.encodeBase64ToString
+
+
+object AuxBrain {
+    private const val GET_SALES_URL = "http://www.auxbrain.com/ei/get_sales"
+    private const val GET_EVENTS_URL = "http://www.auxbrain.com/ei/get_events"
+    private const val GET_CONTRACTS_URL = "http://www.auxbrain.com/ei/get_contracts"
+    private const val COOP_STATUS_URL = "http://www.auxbrain.com/ei/coop_status"
+    private const val FIRST_CONTACT_URL = "http://www.auxbrain.com/ei/first_contact"
+    private const val DAILY_GIFT_URL = "http://www.auxbrain.com/ei/daily_gift_info"
+
+    fun getContracts(handler: (EggInc.GetContractsResponse) -> Unit) = GET_CONTRACTS_URL.httpPost()
+        .response { _, response, _ ->
+            handler(EggInc.GetContractsResponse.parseFrom(response.body().base64Decoded()))
+        }
