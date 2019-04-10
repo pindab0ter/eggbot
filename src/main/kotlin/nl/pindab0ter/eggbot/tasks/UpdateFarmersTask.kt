8daebcca8033ef150356cc48514de1f862b7e617
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2019-04-10 22:34:12 +0200

    Don't attempt an update if there's nothing to update

--- src/main/kotlin/nl/pindab0ter/eggbot/tasks/UpdateFarmersTask.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/tasks/UpdateFarmersTask.kt
@@ -8,10 +8,11 @@ import java.util.*
 
 object UpdateFarmersTask : TimerTask() {
     private val log = KotlinLogging.logger {}
-    override fun run() = transaction {
+    override fun run(): Unit = transaction {
         Farmer.all().sortedBy { it.lastUpdated }
-            .let { it.take((it.size / 6) + 1) }
-            .also { log.info("Updating ${it.joinToString { farmer -> farmer.inGameName }}") }
-            .forEach(Farmer::update)
+            .takeIf { it.isNotEmpty() }
+            ?.let { it.take((it.size / 6) + 1) }
+            ?.also { log.info("Updating ${it.joinToString { farmer -> farmer.inGameName }}") }
+            ?.forEach(Farmer::update)
     }
 }
