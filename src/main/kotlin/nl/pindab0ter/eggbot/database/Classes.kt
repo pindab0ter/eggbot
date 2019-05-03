185becdbfd8848cff06468121e8299e6d0266ecc
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2019-05-03 13:43:37 +0200

    Add another SE exception

--- src/main/kotlin/nl/pindab0ter/eggbot/database/Classes.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/database/Classes.kt
@@ -27,13 +27,19 @@ class Farmer(id: EntityID<String>) : Entity<String>(id) {
     var discordUser by DiscordUser referencedOn Farmers.discordId
     var inGameName by Farmers.inGameName
 
+    // @formatter:off
     @Suppress("PrivatePropertyName")
     private var soulEggs_ by Farmers.soulEggs
-    var soulEggs
-        get() = if (inGameId == "G:1310382811") 39505097770735L else soulEggs_
+    var soulEggs get() = when (inGameId) {
+            "G:1310382811"          -> 39505097770735L
+            "104311114077828861120" ->  2500000000000L
+            else -> soulEggs_
+        }
         set(value) {
             soulEggs_ = value
         }
+    // @formatter:on
+
     var prophecyEggs by Farmers.prophecyEggs
     var soulBonus by Farmers.soulBonus
     var prophecyBonus by Farmers.prophecyBonus
