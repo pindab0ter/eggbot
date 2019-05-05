eba7d9ea8ef5073329b7d69f3602071bd0ae0284
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2019-05-05 09:55:51 +0200

    Optimise dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,20 +20,21 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    compileOnly(kotlin("stdlib-jdk8"))
+    compileOnly("com.google.protobuf", "protobuf-gradle-plugin", "0.8.8")
+
     implementation("com.jagrosh", "jda-utilities-command", "2.1.5")
     implementation("net.dv8tion", "JDA", "3.8.3_463") {
-        exclude("opus-java")
+        exclude("club.minnced", "opus-java")
     }
     implementation("org.jetbrains.exposed", "exposed", "0.13.4")
-    implementation("org.xerial", "sqlite-jdbc", "3.21.0.1")
     implementation("com.github.kittinunf.fuel", "fuel", "2.0.1")
     implementation("com.google.protobuf", "protobuf-java", "3.7.0")
-    implementation("com.google.protobuf", "protobuf-gradle-plugin", "0.8.8")
     implementation("io.github.microutils", "kotlin-logging", "1.6.24")
     implementation("org.quartz-scheduler", "quartz", "2.3.0")
 
-    runtime("org.apache.logging.log4j", "log4j-slf4j-impl", "2.11.2")
+    runtimeOnly("org.apache.logging.log4j", "log4j-slf4j-impl", "2.11.2")
+    runtimeOnly("org.xerial", "sqlite-jdbc", "3.21.0.1")
 }
 
 tasks.withType<KotlinCompile> {
