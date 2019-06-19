fdbc39b8008869728865da4c38f506225da66a19
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2019-06-19 17:00:26 +0200

    Update version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "nl.pindab0ter"
-version = "1.5.0"
+version = "1.5.1"
 
 application {
     mainClassName = "nl.pindab0ter.eggbot.EggBot"
