f68f3dc7fb1e12d61e107766aadb8b9181737bba
Hans van Luttikhuizen <pindab0ter@me.com>
2019-11-26 11:38:08 +0100

    Update dependencies and tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "nl.pindab0ter"
-version = "1.6.0"
+version = "1.7.0"
 
 application {
     mainClassName = "nl.pindab0ter.eggbot.EggBot"
@@ -10,7 +10,7 @@ application {
 plugins {
     idea
     application
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.60"
     id("com.google.protobuf") version "0.8.10"
     id("com.github.ben-manes.versions") version "0.27.0"
 }
@@ -22,21 +22,21 @@ repositories {
 
 dependencies {
     compileOnly(kotlin("stdlib-jdk8"))
-    compileOnly("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.2")
+    compileOnly("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.2-1.3.60")
     compileOnly("com.google.protobuf", "protobuf-gradle-plugin", "0.8.10")
 
     implementation("com.jagrosh", "jda-utilities-commons", "3.0.2")
     implementation("com.jagrosh", "jda-utilities-command", "3.0.2")
-    implementation("net.dv8tion", "JDA", "4.0.0_54") {
+    implementation("net.dv8tion", "JDA", "4.0.0_64") {
         exclude("club.minnced", "opus-java")
     }
-    implementation("org.jetbrains.exposed", "exposed", "0.17.6")
-    implementation("joda-time", "joda-time", "2.10.4")
+    implementation("org.jetbrains.exposed", "exposed", "0.17.7")
+    implementation("joda-time", "joda-time", "2.10.5")
     implementation("com.github.kittinunf.fuel", "fuel", "2.2.1")
-    implementation("com.google.protobuf", "protobuf-java", "3.10.0")
-    implementation("io.github.microutils", "kotlin-logging", "1.7.6")
+    implementation("com.google.protobuf", "protobuf-java", "3.11.0")
+    implementation("io.github.microutils", "kotlin-logging", "1.7.8")
     implementation("org.quartz-scheduler", "quartz", "2.3.2")
-    implementation("ch.obermuhlner", "big-math", "2.2.1")
+    implementation("ch.obermuhlner", "big-math", "2.3.0")
 
     runtimeOnly("org.apache.logging.log4j", "log4j-slf4j-impl", "2.12.1")
     runtimeOnly("org.xerial", "sqlite-jdbc", "3.28.0")
