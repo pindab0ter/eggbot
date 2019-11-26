7b03dbb958becb7ec4e4e7b0c076d842ec4faec4
Hans van Luttikhuizen <pindab0ter@me.com>
2019-11-26 13:37:24 +0100

    Add prestiges to earnings bonus output

--- src/main/kotlin/nl/pindab0ter/eggbot/commands/EarningsBonus.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/commands/EarningsBonus.kt
@@ -2,17 +2,18 @@ package nl.pindab0ter.eggbot.commands
 
 import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
+import kotlinx.coroutines.*
 import mu.KotlinLogging
 import net.dv8tion.jda.api.entities.ChannelType
 import nl.pindab0ter.eggbot.Config
 import nl.pindab0ter.eggbot.Messages
 import nl.pindab0ter.eggbot.commands.categories.FarmersCategory
 import nl.pindab0ter.eggbot.database.DiscordUser
+import nl.pindab0ter.eggbot.database.Farmer
 import nl.pindab0ter.eggbot.network.AuxBrain
-import nl.pindab0ter.eggbot.utilities.PrerequisitesCheckResult
-import nl.pindab0ter.eggbot.utilities.arguments
-import nl.pindab0ter.eggbot.utilities.checkPrerequisites
+import nl.pindab0ter.eggbot.utilities.*
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.math.RoundingMode
 
 object EarningsBonus : Command() {
 
@@ -22,7 +23,6 @@ object EarningsBonus : Command() {
         name = "earnings-bonus"
         aliases = arrayOf("eb", "earningsbonus", "earning-bonus", "earningbonus")
         help = "Shows your EB, EB rank and how much SE till your next rank"
-        arguments = "[compact]"
         category = FarmersCategory
         guildOnly = false
     }
@@ -43,8 +43,6 @@ object EarningsBonus : Command() {
             DiscordUser.findById(event.author.id)?.farmers?.toList()?.sortedBy { it.inGameName }!!
         }
 
-        val compact = event.arguments.any { it.startsWith("c") }
-
         farmers.forEach { farmer ->
             AuxBrain.getFarmerBackup(farmer.inGameId) { (backup, _) ->
                 if (backup == null) "Could not get information on ${farmer.inGameName}".let {
@@ -53,9 +51,83 @@ object EarningsBonus : Command() {
                     return@getFarmerBackup
                 }
 
-                transaction { farmer.update(backup) }
+                GlobalScope.launch(Dispatchers.IO) {
+                    transaction { farmer.update(backup) }
+                }
 
-                Messages.earningsBonus(farmer, compact).let {
+                StringBuilder().apply {
+                    data class Line(
+                        val label: String,
+                        val value: String,
+                        var padding: String = "",
+                        val suffix: String? = null
+                    )
+
+                    val roleLabel = "Role:  "
+                    val role = farmer.role?.name ?: "Unknown"
+                    val earningsBonusLabel = "Earnings bonus:  "
+                    val earningsBonus = farmer.earningsBonus.formatIllions()
+                    val earningsBonusSuffix = " %"
+                    val soulEggsLabel = "Soul Eggs:  "
+                    val soulEggs = farmer.soulEggs.formatIllions()
+                    val soulEggsSuffix = " SE"
+                    val prophecyEggsLabel = "Prophecy Eggs:  "
+                    val prophecyEggs = farmer.prophecyEggs.formatInteger()
+                    val prophecyEggsSuffix = " PE"
+                    val soulBonusLabel = "Soul Food:  "
+                    val soulBonus = "${farmer.soulBonus.formatInteger()}/140"
+                    val prophecyBonusLabel = "Prophecy Bonus:  "
+                    val prophecyBonus = "${farmer.prophecyBonus.formatInteger()}/5"
+                    val prestigesLabel = "Prestiges: "
+                    val prestiges = "${farmer.prestiges}"
+                    val soulEggsToNextLabel = "SE to next rank:  "
+                    val soulEggsToNext = farmer.nextRole
+                        ?.lowerBound
+                        ?.minus(farmer.earningsBonus)
+                        ?.divide(farmer.bonusPerSoulEgg, RoundingMode.HALF_UP)
+                        ?.formatIllions()
+                        ?.let { "+ $it" } ?: "Unknown"
+
+                    append("Earnings bonus for **${farmer.inGameName}**:```\n")
+
+                    val labelsToValues: List<Line> = listOf(
+                        Line(roleLabel, role),
+                        Line(earningsBonusLabel, earningsBonus, suffix = earningsBonusSuffix),
+                        Line(soulEggsLabel, soulEggs, suffix = soulEggsSuffix),
+                        Line(prophecyEggsLabel, prophecyEggs, suffix = prophecyEggsSuffix)
+                    ).run {
+                        if (farmer.soulBonus < 140) this.plus(Line(soulBonusLabel, soulBonus))
+                        else this
+                    }.run {
+                        if (farmer.prophecyBonus < 5) this.plus(Line(prophecyBonusLabel, prophecyBonus))
+                        else this
+                    }.plus(
+                        arrayOf(
+                            Line(prestigesLabel, prestiges),
+                            Line(soulEggsToNextLabel, soulEggsToNext, suffix = soulEggsSuffix)
+                        )
+                    )
+
+                    // TODO: Clean up this code
+
+                    val lines = labelsToValues.map { (label, value, _, suffix) ->
+                        val padding = paddingCharacters(label, labelsToValues.map { it.label }) +
+                                paddingCharacters(value, labelsToValues.map { it.value })
+                        Line(label, value, padding, suffix)
+                    }.let { lines ->
+                        val shortestPadding = lines.map { it.padding }.minBy { it.length }?.length ?: 0
+                        lines.map { (label, value, padding, suffix) ->
+                            Line(label, value, padding.drop(shortestPadding), suffix)
+                        }
+                    }
+
+                    lines.forEach { (label, value, padding, suffix) ->
+                        appendln(label + padding + value + (suffix ?: ""))
+                    }
+
+                    appendln("```")
+
+                }.toString().let {
                     if (event.channel.id == Config.botCommandsChannel) {
                         event.reply(it)
                     } else event.replyInDm(it) {
