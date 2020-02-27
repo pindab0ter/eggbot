32a8c8ddbf2f3569557c3729aa55e877fb3d40e6
pindab0ter <hansvanluttikhuizen@me.com>
2020-02-27 21:40:24 +0100

    Remove Any.unit value

--- src/main/kotlin/nl/pindab0ter/eggbot/commands/Test.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/commands/Test.kt
@@ -2,13 +2,9 @@ package nl.pindab0ter.eggbot.commands
 
 import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import mu.KotlinLogging
 import nl.pindab0ter.eggbot.utilities.ProgressBarUpdater
-import nl.pindab0ter.eggbot.utilities.unit
 import kotlin.coroutines.CoroutineContext
 
 object Test : Command(), CoroutineScope {
@@ -21,17 +17,19 @@ object Test : Command(), CoroutineScope {
     }
 
     @Suppress("FoldInitializerAndIfToElvis")
-    override fun execute(event: CommandEvent) = launch {
-        event.channel.sendTyping().queue()
+    override fun execute(event: CommandEvent) {
+        launch {
+            event.channel.sendTyping().queue()
 
-        val message = event.channel.sendMessage("Progress…").complete()
-        val total = 250
-        val progressBar = ProgressBarUpdater(total, message, true)
-        (1..total).forEach { i ->
-            progressBar.update(i)
-            delay(20)
+            val message = event.channel.sendMessage("Progress…").complete()
+            val total = 250
+            val progressBar = ProgressBarUpdater(total, message, true)
+            (1..total).forEach { i ->
+                progressBar.update(i)
+                delay(20)
+            }
         }
-    }.unit
+    }
 
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Default
--- src/main/kotlin/nl/pindab0ter/eggbot/utilities/Miscellaneous.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/utilities/Miscellaneous.kt
@@ -10,9 +10,6 @@ import java.util.*
 
 val ONE_YEAR get() = Duration(DateTime.now(), DateTime.now().plusYears(1))
 
-val Any?.unit: Unit
-    get() = Unit
-
 fun Body.decodeBase64(): ByteArray = Base64.getDecoder().decode(toByteArray())
 
 fun <T> Iterable<T>.init() = take((count() - 1).coerceAtLeast(0))
