af88985dea308d540eceedc59fa0c1739f6244dc
pindab0ter <hansvanluttikhuizen@me.com>
2020-02-28 10:11:21 +0100

    Hide empty categories from help

--- src/main/kotlin/nl/pindab0ter/eggbot/jda/HelpConsumer.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/jda/HelpConsumer.kt
@@ -9,9 +9,7 @@ import java.util.function.Consumer
 
 object HelpConsumer : Consumer<CommandEvent> {
 
-    override fun accept(event: CommandEvent) = StringBuilder(
-        "`<>` = required argument, `[]` = optional argument\nType the arguments without the brackets.\n"
-    ).apply {
+    override fun accept(event: CommandEvent) = StringBuilder().apply {
 
         fun append(commands: List<Command>) = commands.forEach { command ->
             if (!command.isHidden && (!command.isOwnerCommand || event.isOwner)) {
@@ -24,13 +22,17 @@ object HelpConsumer : Consumer<CommandEvent> {
             }
         }
 
+        appendln("`<>` = required argument, `[]` = optional argument\nType the arguments without the brackets.\n")
+
         commandClient.commands.let { commands ->
             if (commands.any { it.category != null }) {
-                commands.groupBy { it.category }.toList().forEachIndexed { i, (category, commands) ->
-                    append(if (i == 0) "\n" else "\n\n")
-                    append("  __${if (category != null) category.name else "No category"}__:")
-                    append(commands)
-                }
+                commands.groupBy { it.category }.toList()
+                    .filterNot { (_, commands) -> commands.all { command -> command.isHidden } }
+                    .forEachIndexed { i, (category, commands) ->
+                        append(if (i == 0) "\n" else "\n\n")
+                        append("  __${if (category != null) category.name else "No category"}__:")
+                        append(commands)
+                    }
             } else append(commands)
         }
     }.toString().let { helpMessage ->
