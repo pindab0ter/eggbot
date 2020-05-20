cda981061fcc50d9437ed366edbd6a529193ce75
Hans van Luttikhuizen-Ross <hansvanluttikhuizen@me.com>
2020-05-20 10:49:27 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,16 +4,16 @@ group = "nl.pindab0ter"
 version = "1.8.0"
 
 application {
+    applicationName = "EggBot"
     mainClassName = "nl.pindab0ter.eggbot.EggBot"
 }
 
 plugins {
     idea
     application
-    kotlin("jvm") version "1.3.61"
-    id("com.google.protobuf") version "0.8.12"
+    kotlin("jvm") version "1.3.72"
     id("com.github.ben-manes.versions") version "0.28.0"
-    id("com.toasttab.protokt") version "0.4.1"
+    id("com.toasttab.protokt") version "0.4.2"
 }
 
 repositories {
@@ -24,21 +24,21 @@ repositories {
 dependencies {
     compileOnly(kotlin("stdlib-jdk8"))
     compileOnly("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.2-1.3.60")
-    implementation("com.jagrosh", "jda-utilities-command", "3.0.2")
-    implementation("net.dv8tion", "JDA", "4.1.1_109") {
+    implementation("io.github.microutils", "kotlin-logging", "1.7.9")
+    implementation("joda-time", "joda-time", "2.10.6")
+    implementation("com.github.kittinunf.fuel", "fuel", "2.2.2")
+    implementation("org.jetbrains.exposed", "exposed", "0.17.7")
+    implementation("com.jagrosh", "jda-utilities-command", "3.0.3")
+    implementation("net.dv8tion", "JDA", "4.1.1_153") {
         exclude("club.minnced", "opus-java")
     }
-    implementation("org.jetbrains.exposed", "exposed", "0.17.7")
-    implementation("joda-time", "joda-time", "2.10.5")
-    implementation("com.github.kittinunf.fuel", "fuel", "2.2.1")
-    implementation("com.google.protobuf", "protobuf-java", "3.11.4")
-    implementation("io.github.microutils", "kotlin-logging", "1.7.8")
-    implementation("org.quartz-scheduler", "quartz", "2.3.2")
-    implementation("ch.obermuhlner", "big-math", "2.3.0")
     implementation("org.campagnelab.ext", "jsap", "3.0.0")
+    implementation("ch.obermuhlner", "big-math", "2.3.0")
+    implementation("org.quartz-scheduler", "quartz", "2.3.2")
 
-    runtimeOnly("org.apache.logging.log4j", "log4j-slf4j-impl", "2.13.0")
-    runtimeOnly("org.xerial", "sqlite-jdbc", "3.30.1")
+    runtimeOnly("com.google.protobuf", "protobuf-java", "3.12.0")
+    runtimeOnly("org.apache.logging.log4j", "log4j-slf4j-impl", "2.13.3")
+    runtimeOnly("org.xerial", "sqlite-jdbc", "3.31.1")
 }
 
 tasks.withType<KotlinCompile> {
