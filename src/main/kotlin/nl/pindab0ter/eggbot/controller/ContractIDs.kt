8419f57d889d67ceaea4967e580aa2721128258a
Hans van Luttikhuizen-Ross <hansvanluttikhuizen@me.com>
2020-08-26 13:10:13 +0200

    Update and refactor ContractIDs message

--- src/main/kotlin/nl/pindab0ter/eggbot/controller/ContractIDs.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/controller/ContractIDs.kt
@@ -1,16 +1,11 @@
 package nl.pindab0ter.eggbot.controller
 
-import com.auxbrain.ei.Contract
 import com.jagrosh.jdautilities.command.CommandEvent
 import com.martiansoftware.jsap.JSAPResult
-import nl.pindab0ter.eggbot.EggBot.eggsToEmotes
 import nl.pindab0ter.eggbot.controller.categories.ContractsCategory
 import nl.pindab0ter.eggbot.jda.EggBotCommand
 import nl.pindab0ter.eggbot.model.AuxBrain
-import nl.pindab0ter.eggbot.helpers.asDaysHoursAndMinutes
-import nl.pindab0ter.eggbot.helpers.toDateTime
-import org.joda.time.DateTime
-import org.joda.time.Duration
+import nl.pindab0ter.eggbot.view.contractIDsResponse
 
 object ContractIDs : EggBotCommand() {
 
@@ -30,36 +25,10 @@ object ContractIDs : EggBotCommand() {
                 .groupBy { it.coopAllowed }
                 .let { it[false].orEmpty() to it[true].orEmpty() }
 
-            if (soloContracts.plus(coopContracts).isNotEmpty()) {
-                event.reply(StringBuilder("IDs for currently active contracts:").apply {
-                    if (soloContracts.isNotEmpty()) {
-                        append("\n  __Solo contracts__:\n")
-                        append(soloContracts.printContracts())
-                    }
-                    if (coopContracts.isNotEmpty()) {
-                        append("\n  __Co-op contracts__:\n")
-                        append(coopContracts.printContracts())
-                    }
-                }.toString())
-            } else event.replyWarning("There are currently no active contracts")
+            if (soloContracts.isNotEmpty() && coopContracts.isNotEmpty())
+                event.reply(contractIDsResponse(soloContracts, coopContracts))
+            else event.replyWarning("There are currently no active contracts")
         }
     }
-
-    // TODO: Add info about requirements
-    private fun List<Contract>.printContracts(): String = StringBuilder().apply {
-        this@printContracts.forEach { contract ->
-            append("**`${contract.id}`**: ")
-            append("${contract.name} ")
-            append(eggsToEmotes[contract.egg]?.asMention ?: "(${contract.egg.name})")
-            append(", valid for ")
-            append(
-                Duration(DateTime.now(), contract.expirationTime.toDateTime())
-                    .toPeriod()
-                    .normalizedStandard()
-                    .asDaysHoursAndMinutes()
-            )
-            appendLine()
-        }
-    }.toString()
 }
 
---
+++ src/main/kotlin/nl/pindab0ter/eggbot/view/ContractIDs.kt
@@ -0,0 +1,38 @@
+package nl.pindab0ter.eggbot.view
+
+import com.auxbrain.ei.Contract
+import nl.pindab0ter.eggbot.EggBot
+import nl.pindab0ter.eggbot.helpers.asDaysHoursAndMinutes
+import nl.pindab0ter.eggbot.helpers.toDateTime
+import org.joda.time.DateTime
+import org.joda.time.Duration
+
+
+fun contractIDsResponse(soloContracts: List<Contract>, coopContracts: List<Contract>): String = buildString {
+    appendLine("IDs for currently active contracts:")
+
+    fun List<Contract>.printContracts(): String = StringBuilder().apply {
+        this@printContracts.forEach { contract ->
+            append("**`${contract.id}`**: ")
+            append("${contract.name} ")
+            append(EggBot.eggsToEmotes[contract.egg]?.asMention ?: "(${contract.egg.name})")
+            append(", _valid for ")
+            append(
+                Duration(DateTime.now(), contract.expirationTime.toDateTime())
+                    .toPeriod()
+                    .normalizedStandard()
+                    .asDaysHoursAndMinutes()
+            )
+            appendLine("_")
+        }
+    }.toString()
+
+    if (soloContracts.isNotEmpty()) {
+        append("\n  __Solo contracts__:\n")
+        append(soloContracts.printContracts())
+    }
+    if (coopContracts.isNotEmpty()) {
+        append("\n  __Co-op contracts__:\n")
+        append(coopContracts.printContracts())
+    }
+}
