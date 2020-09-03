9ac77082c8195cea36726362566938a8f17a8ff5
Hans van Luttikhuizen-Ross <hansvanluttikhuizen@me.com>
2020-09-03 09:15:00 +0200

    Update coop state finished check

--- src/main/kotlin/nl/pindab0ter/eggbot/model/simulation/new/CoopContractState.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/model/simulation/new/CoopContractState.kt
@@ -26,7 +26,7 @@ data class CoopContractState(
     val goalsReached: Int get() = goals.count { (_, moment) -> moment != null }
     val tokensAvailable: Int get() = farmers.sumBy { farmer -> farmer.constants.tokensAvailable }
     val tokensSpent: Int get() = farmers.sumBy { farmer -> farmer.constants.tokensSpent }
-    val finished: Boolean get() = farmers.all { farmer -> farmer.initialState == farmer.finalState }
+    val finished: Boolean get() = goals.all { goal -> goal.moment != null }
 
     constructor(
         contract: Contract,
