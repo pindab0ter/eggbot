1afee4d9f79380ac134289501dda062c35e603c8
Hans van Luttikhuizen-Ross <hansvanluttikhuizen@me.com>
2020-09-03 08:38:11 +0200

    Rename role_id

--- src/main/kotlin/nl/pindab0ter/eggbot/database/Coops.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/database/Coops.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.dao.IntIdTable
 object Coops : IntIdTable() {
     val name = text("name")
     val contract = text("contract_id")
-    val role_id = text("role_id").nullable()
+    val roleId = text("role_id").nullable()
 
     init {
         this.index(true, name, contract)
--- src/main/kotlin/nl/pindab0ter/eggbot/model/database/Coop.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/model/database/Coop.kt
@@ -12,7 +12,7 @@ import java.math.BigDecimal
 class Coop(id: EntityID<Int>) : IntEntity(id) {
     var name by Coops.name
     var contract by Coops.contract
-    var roleId by Coops.role_id
+    var roleId by Coops.roleId
 
     var farmers by Farmer via CoopFarmers
 
