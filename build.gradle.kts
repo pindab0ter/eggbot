3ffcc89a0448df74105669ebd787351d970c2167
Hans van Luttikhuizen-Ross <hansvanluttikhuizen@me.com>
2020-11-07 21:11:20 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ plugins {
     idea
     application
     kotlin("jvm") version "1.4.10"
-    id("com.github.ben-manes.versions") version "0.29.0"
-    id("com.toasttab.protokt") version "0.5.2"
+    id("com.github.ben-manes.versions") version "0.33.0"
+    id("com.toasttab.protokt") version "0.5.3"
 }
 
 repositories {
@@ -23,26 +23,26 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.9")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.4.0")
     implementation("org.apache.logging.log4j", "log4j-api-kotlin", "1.0.0")
-    implementation("joda-time", "joda-time", "2.10.6")
+    implementation("joda-time", "joda-time", "2.10.8")
     implementation("com.github.kittinunf.fuel", "fuel", "2.3.0")
     implementation("org.jetbrains.exposed", "exposed", "0.17.7")
     implementation("com.jagrosh", "jda-utilities-command", "3.0.4")
-    implementation("net.dv8tion", "JDA", "4.2.0_208") {
+    implementation("net.dv8tion", "JDA", "4.2.0_212") {
         exclude("club.minnced", "opus-java")
     }
     implementation("org.campagnelab.ext", "jsap", "3.0.0")
     implementation("ch.obermuhlner", "big-math", "2.3.0")
     implementation("org.quartz-scheduler", "quartz", "2.3.2")
 
-    runtimeOnly("com.google.protobuf", "protobuf-java", "3.13.0")
+    runtimeOnly("com.google.protobuf", "protobuf-java", "4.0.0-rc-2")
     runtimeOnly("org.apache.logging.log4j", "log4j-slf4j-impl", "2.13.3")
     runtimeOnly("org.xerial", "sqlite-jdbc", "3.32.3.2")
 
-    testImplementation("io.kotest", "kotest-runner-junit5-jvm", "4.2.6")
-    implementation("io.kotest", "kotest-assertions-core-jvm", "4.2.6")
-    testImplementation("io.kotest", "kotest-property-jvm", "4.2.6")
+    testImplementation("io.kotest", "kotest-runner-junit5-jvm", "4.3.0")
+    implementation("io.kotest", "kotest-assertions-core-jvm", "4.3.0")
+    testImplementation("io.kotest", "kotest-property-jvm", "4.3.0")
     testImplementation("io.mockk", "mockk", "1.10.2")
 }
 
