f6929015645d27e890ed3d28df0bc9138d986f1c
pindab0ter <hansvanluttikhuizen@me.com>
2020-12-29 16:54:52 +0100

    Add documentation to deploy script and fix bug

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Usage
+#  $ ./deploy.sh ssh-config-name folder-and-pane-name
+# * ssh-config-name: The name of the SSH config set up to connect to the server to deploy to
+# * folder-and-pane-name: The name of the folder in the home directory and tmux pane (must be the same)
+
 set -o errexit -o nounset -o pipefail
 ./gradlew installDist
 cd build/install/EggBot
@@ -13,5 +19,5 @@ rsync \
   -e ssh \
   ./bin \
   ./lib \
-  "$1":~/EggBot
+  "$1":~/"$2"
 ssh "$1" 'tmux send-keys -t "$2" C-c "bin/EggBot" Enter'
