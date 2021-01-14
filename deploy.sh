9f34463b40efee71603b4d4c7da1595ba079854e
pindab0ter <hansvanluttikhuizen@me.com>
2021-01-14 15:10:51 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Usage
-#  $ ./deploy.sh ssh-config-name folder-and-pane-name
+# $ ./deploy.sh ssh-config-name folder-and-pane-name
 # * ssh-config-name: The name of the SSH config set up to connect to the server to deploy to
-# * folder-and-pane-name: The name of the folder in the home directory and tmux pane (must be the same)
+# * name: The name of the folder in the home directory and tmux window (must be the same)
 
 set -o errexit -o nounset -o pipefail
 ./gradlew installDist
 cd build/install/EggBot
+ssh_config=$1
+name=$2
+echo "Updating ${name} on ${ssh_config}…"
 rsync \
   --recursive \
   --checksum \
@@ -19,5 +22,9 @@ rsync \
   -e ssh \
   ./bin \
   ./lib \
-  "$1":~/"$2"
-ssh "$1" 'tmux send-keys -t "$2" C-c "bin/EggBot" Enter'
+  "$ssh_config":~/"$name"
+# shellcheck disable=SC2087
+ssh -t "$ssh_config" <<EOF
+  pkill -f /$name
+  tmux new-window -n $name -c $name bin/EggBot
+EOF
