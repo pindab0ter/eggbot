f61eccf2ddb898d4cee41c5e8218bf24db8fce34
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2021-02-15 10:12:17 +0100

    Fix bugs involving Migrate command

--- src/main/kotlin/nl/pindab0ter/eggbot/controller/Migrate.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/controller/Migrate.kt
@@ -1,12 +1,15 @@
 package nl.pindab0ter.eggbot.controller
 
+import com.auxbrain.ei.Backup
 import com.jagrosh.jdautilities.command.CommandEvent
 import com.martiansoftware.jsap.JSAP.REQUIRED
 import com.martiansoftware.jsap.JSAPResult
 import com.martiansoftware.jsap.UnflaggedOption
 import net.dv8tion.jda.api.Permission.MESSAGE_MANAGE
 import net.dv8tion.jda.api.entities.ChannelType
+import nl.pindab0ter.eggbot.controller.Migrate.Result.*
 import nl.pindab0ter.eggbot.controller.categories.FarmersCategory
+import nl.pindab0ter.eggbot.database.CoopFarmers.farmer
 import nl.pindab0ter.eggbot.jda.EggBotCommand
 import nl.pindab0ter.eggbot.model.AuxBrain
 import nl.pindab0ter.eggbot.model.database.DiscordUser
@@ -37,54 +40,60 @@ object Migrate : EggBotCommand() {
         init()
     }
 
-    override fun execute(event: CommandEvent, parameters: JSAPResult) {
-        event.author.openPrivateChannel().queue { it.sendTyping().complete() }
+    sealed class Result {
+        object NotRegistered : Result()
+        object AlreadyMigrated : Result()
+        object NotFound : Result()
+        class NoMatch(val backup: Backup, val farmers: List<Farmer>) : Result()
+        class Success(val farmer: Farmer) : Result()
+        object FailedToCreate : Result()
+    }
 
-        if (event.isFromType(ChannelType.TEXT)) {
-            return if (Register.botPermissions.contains(MESSAGE_MANAGE)) event.replyInDmAndLog("Migrating is only allowed in DMs to protect your in-game ID. Please give it a go here!")
+    override fun execute(event: CommandEvent, parameters: JSAPResult) {
+        event.author.openPrivateChannel().queue { it.sendTyping().queue() }
+
+        if (event.isFromType(ChannelType.TEXT)) return if (Register.botPermissions.contains(MESSAGE_MANAGE))
+            event.replyInDmAndLog("Migrating is only allowed in DMs to protect your in-game ID. Please give it a go here!")
                 .also { event.message.delete().queue() }
-            else event.replyInDmAndLog("Migrating is only allowed in DMs to protect your in-game ID. Please give it a go here and delete your previous message!")
-        }
+        else event.replyInDmAndLog("Migrating is only allowed in DMs to protect your in-game ID. Please give it a go here and delete your previous message!")
 
         val eggIncId = parameters.getString(EGG_INC_ID).toUpperCase()
 
-        val farmer: Farmer? = transaction {
+        val result: Result = transaction {
             val discordUser: DiscordUser = DiscordUser.findById(event.author.id)
-                ?: return@transaction null.also { event.replyAndLogWarning("You are not registered.") }
+                ?: return@transaction NotRegistered
 
-            if (discordUser.farmers.all { farmer -> farmer.inGameId.startsWith("EI") }) return@transaction null.also {
-                event.replyAndLogSuccess("Your in-game ID has already been updated.")
-            }
+            if (discordUser.farmers.all { farmer -> farmer.inGameId.startsWith("EI") })
+                return@transaction AlreadyMigrated
 
             val backup = AuxBrain.getFarmerBackup(eggIncId)
 
-            if (backup?.game == null || backup.stats == null) return@transaction null.also {
-                event.replyAndLogWarning("""
-                    No account found with in-game ID `$eggIncId`. Did you enter your new Egg, Inc. ID correctly?
-                    Type `${event.client.textualPrefix}${Register.name} --help` for more info, hints and tips on how to use this command.
-                    """.trimIndent()
-                )
-            }
+            if (backup?.game == null || backup.stats == null) return@transaction NotFound
+
 
             val oldFarmer = discordUser.farmers.find { farmer ->
                 farmer.inGameName == backup.userName
-            } ?: return@transaction null.also {
-                event.replyAndLogWarning("""
-                    The account for the given Egg, Inc. ID (${backup.userName}) does not match any of the accounts you are registered with (${discordUser.farmers.joinToString { it.inGameName }}).
-                    If you think this is an error, please contact the bot maintainer.
-                    """.trimIndent()
-                )
-            }
+            } ?: return@transaction NoMatch(backup, discordUser.farmers.toList())
 
             oldFarmer.delete()
 
-            return@transaction Farmer.new(discordUser, backup)
+            Farmer.new(discordUser, backup)?.let(::Success) ?: FailedToCreate
         }
 
-        if (farmer != null) {
-            event.replyAndLogSuccess("You have successfully migrated ${farmer.inGameName}!")
-        } else {
-            event.replyAndLogError("Failed to migrate. Please contact the bot maintainer.")
+        when (result) {
+            is NotRegistered -> event.replyAndLogWarning("You are not registered.")
+            is AlreadyMigrated -> event.replyAndLogSuccess("Your in-game ID has already been updated.")
+            is NotFound -> event.replyAndLogWarning("""
+                    No account found with in-game ID `$eggIncId`. Did you enter your new Egg, Inc. ID correctly?
+                    Type `${event.client.textualPrefix}${Register.name} --help` for more info, hints and tips on how to use this command.
+                    """.trimIndent()
+            )
+            is NoMatch -> event.replyAndLogWarning("""
+                    The account for the given Egg, Inc. ID (${result.backup.userName}) does not match any of the accounts you are registered with (${result.farmers.joinToString { it.inGameName }}).
+                    If you think this is an error, please contact the bot maintainer.
+                    """.trimIndent())
+            is Success -> event.replyAndLogSuccess("You have successfully migrated ${result.farmer.inGameName}!")
+            is FailedToCreate -> event.replyAndLogError("Failed to migrate. Please contact the bot maintainer.")
         }
     }
 }
