85b9e3935891075bc460f862a74daa17f6f2250f
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2021-11-19 23:12:28 +0100

    Update Scratch file

--- src/main/kotlin/nl/pindab0ter/eggbot/utilities/Scratch.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/utilities/Scratch.kt
@@ -1,4 +1,22 @@
 package nl.pindab0ter.eggbot.utilities
 
-fun main() {
-}
+import com.kotlindiscord.kord.extensions.ExtensibleBot
+import mu.KotlinLogging
+import nl.pindab0ter.eggbot.connectToDatabase
+import nl.pindab0ter.eggbot.model.Config
+import kotlin.system.exitProcess
+
+suspend fun main() = ExtensibleBot(Config.botToken) {
+    val logger = KotlinLogging.logger {}
+
+    hooks {
+        beforeStart {
+            connectToDatabase()
+
+            // Scratch here
+
+            exitProcess(0)
+        }
+    }
+}.start()
+
