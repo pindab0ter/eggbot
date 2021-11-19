234a36beece84aaf65cb59fd9cd53c79196d6d06
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2021-11-19 22:16:50 +0100

    Implement clear roll call command

--- src/main/kotlin/nl/pindab0ter/eggbot/commands/groups/AdminRollCallGroup.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/commands/groups/AdminRollCallGroup.kt
@@ -12,7 +12,10 @@ import dev.kord.common.entity.Permission.ManageChannels
 import dev.kord.common.entity.Permission.ManageRoles
 import dev.kord.core.behavior.createRole
 import dev.kord.core.behavior.createTextChannel
+import dev.kord.rest.request.RestRequestException
 import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
+import nl.pindab0ter.eggbot.commands.groups.Type.*
 import nl.pindab0ter.eggbot.converters.contract
 import nl.pindab0ter.eggbot.helpers.*
 import nl.pindab0ter.eggbot.model.Config
@@ -25,9 +28,20 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 private val DEFAULT_ROLE_COLOR = Color(15, 212, 57) // #0FD439
 
+private data class DeletionStatus(
+    val type: Type,
+    val deleted: Boolean,
+)
+
+private enum class Type {
+    CHANNEL, ROLE
+}
+
 @KordPreview
 val rollCallGroup: suspend SlashGroup.() -> Unit = {
     description = "Manage roll calls"
+
+    val logger = KotlinLogging.logger { }
 
     class CreateRollCallArguments : Arguments() {
         val contract: Contract by contract()
@@ -57,7 +71,8 @@ val rollCallGroup: suspend SlashGroup.() -> Unit = {
         action {
             // Check if roles or channels can be created if required
             if (configuredGuild == null && (arguments.createRoles || arguments.createChannels)) return@action respond {
-                content = "${Config.emojiWarning} Could not get server info. Please try without creating roles or channels or else please contact the bot maintainer."
+                content =
+                    "${Config.emojiWarning} Could not get server info. Please try without creating roles or channels or else please contact the bot maintainer."
             }.discard()
 
             val coops = transaction {
@@ -123,11 +138,68 @@ val rollCallGroup: suspend SlashGroup.() -> Unit = {
                 content = "No co-ops found for _${arguments.contract.name}_."
             }.discard()
 
-            // TODO: Remove channels
-            // TODO: Delete all roles
-            // TODO: Delete all channels
-            // TODO: Delete all database entries
-            // TODO: Response
+
+            val statuses = coops
+                .map { coop: Coop -> coop to mutableSetOf<DeletionStatus>() }
+                .map { (coop: Coop, statuses: MutableSet<DeletionStatus>) ->
+                    val coopName = coop.name
+
+                    try {
+                        coop.channel?.delete("Roll Call for ${arguments.contract.name} cleared by ${user.asUser().username}")
+                        statuses.add(DeletionStatus(CHANNEL, true))
+                    } catch (exception: RestRequestException) {
+                        statuses.add(DeletionStatus(CHANNEL, false))
+                        logger.warn { "Failed to delete channel for co-op $coopName: ${exception.localizedMessage}" }
+                    }
+
+                    try {
+                        coop.role?.delete("Roll Call for ${arguments.contract.name} cleared by ${user.asUser().username}")
+                        statuses.add(DeletionStatus(ROLE, true))
+                    } catch (exception: RestRequestException) {
+                        statuses.add(DeletionStatus(ROLE, false))
+                        logger.warn { "Failed to delete role for co-op $coopName: ${exception.localizedMessage}" }
+                    }
+
+                    transaction { coop.delete() }
+
+                    coopName to statuses.toSet()
+                }
+
+            respond {
+                content = buildString {
+                    val successfullyDeletedChannels = statuses.count { (_, statuses: Set<DeletionStatus>) ->
+                        statuses.any { deletionStatus: DeletionStatus ->
+                            deletionStatus.type == CHANNEL && deletionStatus.deleted
+                        }
+                    }
+                    val successfullyDeletedRoles = statuses.count { (_, statuses: Set<DeletionStatus>) ->
+                        statuses.any { deletionStatus: DeletionStatus ->
+                            deletionStatus.type == ROLE && deletionStatus.deleted
+                        }
+                    }
+                    appendLine("Successfully deleted $successfullyDeletedChannels channels and $successfullyDeletedRoles roles.")
+
+                    statuses
+                        .map { (coopName, statuses) ->
+                            coopName to statuses
+                                .filterNot(DeletionStatus::deleted)
+                                .map { deletionStatus -> deletionStatus.type }
+                                .sorted()
+                        }
+                        .filter { (_, statuses) -> statuses.isNotEmpty() }
+                        .sortedWith(compareBy { it.first })
+                        .let { failedToDelete ->
+                            appendLine("Failed to delete:")
+                            failedToDelete.forEach { (coopName, types) ->
+                                append("For `$coopName`: ")
+                                when (types.size) {
+                                    1 -> append(types.first().name.lowercase())
+                                    else -> types.joinToString(" and ") { type -> type.name.lowercase() }
+                                }
+                            }
+                        }
+                }
+            }
         }
     }
 }
--- src/main/kotlin/nl/pindab0ter/eggbot/model/database/Coop.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/model/database/Coop.kt
@@ -1,6 +1,11 @@
 package nl.pindab0ter.eggbot.model.database
 
 import dev.kord.common.entity.Snowflake
+import dev.kord.core.entity.Role
+import dev.kord.core.entity.channel.Channel
+import kotlinx.coroutines.runBlocking
+import nl.pindab0ter.eggbot.helpers.configuredGuild
+import nl.pindab0ter.eggbot.model.Config
 import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
@@ -11,29 +16,39 @@ class Coop(id: EntityID<Int>) : IntEntity(id) {
     var name by Coops.name
     var contractId by Coops.contractId
     var leader: Farmer? by Farmer optionalReferencedOn Coops.leaderId
+
     private var _roleId by Coops.roleId
     var roleId: Snowflake?
         get() = this._roleId?.let { Snowflake(it) }
         set(snowflake) {
             _roleId = snowflake?.asString
         }
+    val role: Role?
+        get() = this@Coop.roleId?.let {
+            runBlocking { configuredGuild?.getRoleOrNull(it) }
+        }
+
     private var _channelId by Coops.channelId
     var channelId: Snowflake?
         get() = this._channelId?.let { Snowflake(it) }
         set(snowflake) {
             _channelId = snowflake?.asString
         }
-    var createdAt by Coops.createdAt
-    var updatedAt by Coops.updated_at
+    val channel: Channel?
+        get() = this@Coop.channelId?.let {
+            runBlocking { configuredGuild?.getChannelOrNull(it) }
+        }
 
     var farmers by Farmer via CoopFarmers
-
     val hasLeader: Boolean get() = leader != null
     val activeEarningsBonus: BigDecimal
         get() = farmers.sumOf { farmer ->
             if (farmer.isActive) farmer.earningsBonus
             else BigDecimal.ZERO
         }
+
+    var createdAt by Coops.createdAt
+    var updatedAt by Coops.updated_at
 
     companion object : IntEntityClass<Coop>(Coops)
 }
