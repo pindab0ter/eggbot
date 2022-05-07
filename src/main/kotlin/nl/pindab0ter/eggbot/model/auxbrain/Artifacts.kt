43abbf41e7b6777bb06eaa4a52925cb891c2c480
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2022-05-07 23:39:47 +0200

    Fill out missing artifact values

--- src/main/kotlin/nl/pindab0ter/eggbot/model/auxbrain/Artifacts.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/model/auxbrain/Artifacts.kt
@@ -50,43 +50,35 @@ private val artifactMultipliers = hashMapOf(
 
     Identifier(CHALICE,    INFERIOR, COMMON)    to BigDecimal("1.05"),
     Identifier(CHALICE,    LESSER,   COMMON)    to BigDecimal("1.10"),
-    Identifier(CHALICE,    LESSER,   RARE)      to BigDecimal("1.12"),
-    Identifier(CHALICE,    LESSER,   EPIC)      to BigDecimal("1.14"),
+    Identifier(CHALICE,    LESSER,   EPIC)      to BigDecimal("1.15"),
     Identifier(CHALICE,    NORMAL,   COMMON)    to BigDecimal("1.20"),
     Identifier(CHALICE,    NORMAL,   RARE)      to BigDecimal("1.23"),
-    Identifier(CHALICE,    NORMAL,   EPIC)      to BigDecimal("1.30"),
+    Identifier(CHALICE,    NORMAL,   EPIC)      to BigDecimal("1.25"),
     Identifier(CHALICE,    GREATER,  COMMON)    to BigDecimal("1.30"),
-    // Identifier(CHALICE,    GREATER,  RARE)      to BigDecimal("?"),
-    // Identifier(CHALICE,    GREATER,  EPIC)      to BigDecimal("?"),
-    // Identifier(CHALICE,    GREATER,  LEGENDARY) to BigDecimal("?"),
+    Identifier(CHALICE,    GREATER,  EPIC)      to BigDecimal("1.35"),
+    Identifier(CHALICE,    GREATER,  LEGENDARY) to BigDecimal("1.40"),
 
     // Gussets
 
     Identifier(GUSSET,               INFERIOR, COMMON)    to BigDecimal("1.05"),
     Identifier(GUSSET,               LESSER,   COMMON)    to BigDecimal("1.10"),
-    // Identifier(GUSSET,               LESSER,   RARE)      to BigDecimal("?"),
     Identifier(GUSSET,               LESSER,   EPIC)      to BigDecimal("1.12"),
-    Identifier(GUSSET,               NORMAL,   COMMON)    to BigDecimal("1.14"),
-    Identifier(GUSSET,               NORMAL,   RARE)      to BigDecimal("1.15"),
-    // Identifier(GUSSET,               NORMAL,   EPIC)      to BigDecimal("?"),
+    Identifier(GUSSET,               NORMAL,   COMMON)    to BigDecimal("1.15"),
+    Identifier(GUSSET,               NORMAL,   RARE)      to BigDecimal("1.16"),
     Identifier(GUSSET,               GREATER,  COMMON)    to BigDecimal("1.20"),
-    // Identifier(GUSSET,               GREATER,  RARE)      to BigDecimal("?"),
-    // Identifier(GUSSET,               GREATER,  EPIC)      to BigDecimal("?"),
-    Identifier(GUSSET,               GREATER,  LEGENDARY) to BigDecimal("1.30"),
+    Identifier(GUSSET,               GREATER,  EPIC)      to BigDecimal("1.22"),
+    Identifier(GUSSET,               GREATER,  LEGENDARY) to BigDecimal("1.25"),
 
     // Interstellar Compass
 
     Identifier(INTERSTELLAR_COMPASS, INFERIOR, COMMON)    to BigDecimal("1.05"),
     Identifier(INTERSTELLAR_COMPASS, LESSER,  COMMON)     to BigDecimal("1.10"),
-    // Identifier(INTERSTELLAR_COMPASS, LESSER,  RARE)       to BigDecimal("?"),
-    // Identifier(INTERSTELLAR_COMPASS, LESSER,  EPIC)       to BigDecimal("?"),
     Identifier(INTERSTELLAR_COMPASS, NORMAL,  COMMON)     to BigDecimal("1.20"),
     Identifier(INTERSTELLAR_COMPASS, NORMAL,  RARE)       to BigDecimal("1.22"),
-    // Identifier(INTERSTELLAR_COMPASS, NORMAL,  EPIC)       to BigDecimal("?"),
     Identifier(INTERSTELLAR_COMPASS, GREATER, COMMON)     to BigDecimal("1.30"),
-    // Identifier(INTERSTELLAR_COMPASS, GREATER, RARE)       to BigDecimal("?"),
-    // Identifier(INTERSTELLAR_COMPASS, GREATER, EPIC)       to BigDecimal("?"),
-    // Identifier(INTERSTELLAR_COMPASS, GREATER, LEGENDARY)  to BigDecimal("?"),
+    Identifier(INTERSTELLAR_COMPASS, GREATER, RARE)       to BigDecimal("1.35"),
+    Identifier(INTERSTELLAR_COMPASS, GREATER, EPIC)       to BigDecimal("1.40"),
+    Identifier(INTERSTELLAR_COMPASS, GREATER, LEGENDARY)  to BigDecimal("1.50"),
 
     // Life Stones
 
@@ -99,14 +91,13 @@ private val artifactMultipliers = hashMapOf(
     Identifier(QUANTUM_METRONOME,    INFERIOR, COMMON)    to BigDecimal("1.05"),
     Identifier(QUANTUM_METRONOME,    LESSER,   COMMON)    to BigDecimal("1.10"),
     Identifier(QUANTUM_METRONOME,    LESSER,   RARE)      to BigDecimal("1.12"),
-    // Identifier(QUANTUM_METRONOME,    LESSER,   EPIC)      to BigDecimal("?"),
-    Identifier(QUANTUM_METRONOME,    NORMAL,   COMMON)    to BigDecimal("1.14"),
+    Identifier(QUANTUM_METRONOME,    NORMAL,   COMMON)    to BigDecimal("1.15"),
     Identifier(QUANTUM_METRONOME,    NORMAL,   RARE)      to BigDecimal("1.17"),
-    // Identifier(QUANTUM_METRONOME,    NORMAL,   EPIC)      to BigDecimal("?"),
-    // Identifier(QUANTUM_METRONOME,    GREATER,  COMMON)    to BigDecimal("?"),
-    // Identifier(QUANTUM_METRONOME,    GREATER,  RARE)      to BigDecimal("?"),
-    // Identifier(QUANTUM_METRONOME,    GREATER,  EPIC)      to BigDecimal("?"),
-    // Identifier(QUANTUM_METRONOME,    GREATER,  LEGENDARY) to BigDecimal("?"),
+    Identifier(QUANTUM_METRONOME,    NORMAL,   EPIC)      to BigDecimal("1.20"),
+    Identifier(QUANTUM_METRONOME,    GREATER,  COMMON)    to BigDecimal("1.25"),
+    Identifier(QUANTUM_METRONOME,    GREATER,  RARE)      to BigDecimal("1.27"),
+    Identifier(QUANTUM_METRONOME,    GREATER,  EPIC)      to BigDecimal("1.30"),
+    Identifier(QUANTUM_METRONOME,    GREATER,  LEGENDARY) to BigDecimal("1.35"),
 
     // Quantum Stones
 
@@ -117,9 +108,13 @@ private val artifactMultipliers = hashMapOf(
     // Tachyon Deflectors
 
     Identifier(TACHYON_DEFLECTOR,    INFERIOR, COMMON)    to BigDecimal("1.05"),
-    Identifier(TACHYON_DEFLECTOR,    LESSER,   COMMON)    to BigDecimal("1.10"),
+    Identifier(TACHYON_DEFLECTOR,    LESSER,   COMMON)    to BigDecimal("1.08"),
     Identifier(TACHYON_DEFLECTOR,    NORMAL,   COMMON)    to BigDecimal("1.12"),
-    Identifier(TACHYON_DEFLECTOR,    GREATER,  COMMON)    to BigDecimal("1.14"),
+    Identifier(TACHYON_DEFLECTOR,    NORMAL,   RARE)      to BigDecimal("1.13"),
+    Identifier(TACHYON_DEFLECTOR,    GREATER,  COMMON)    to BigDecimal("1.15"),
+    Identifier(TACHYON_DEFLECTOR,    GREATER,  RARE)      to BigDecimal("1.17"),
+    Identifier(TACHYON_DEFLECTOR,    GREATER,  EPIC)      to BigDecimal("1.19"),
+    Identifier(TACHYON_DEFLECTOR,    GREATER,  LEGENDARY) to BigDecimal("1.20"),
 
     // Tachyon Stones
 
