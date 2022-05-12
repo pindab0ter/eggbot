e2a5b81cd95e053356665022ecda6177c425e2f0
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2022-05-12 22:44:21 +0200

    Add farmer lookup for whois command

---
+++ src/main/kotlin/nl/pindab0ter/eggbot/converters/FarmerConverter.kt
@@ -0,0 +1,113 @@
+package nl.pindab0ter.eggbot.converters
+
+
+import com.kotlindiscord.kord.extensions.commands.Argument
+import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.CommandContext
+import com.kotlindiscord.kord.extensions.commands.application.slash.converters.ChoiceConverter
+import com.kotlindiscord.kord.extensions.commands.converters.ConverterToOptional
+import com.kotlindiscord.kord.extensions.commands.converters.OptionalConverter
+import com.kotlindiscord.kord.extensions.commands.converters.SingleConverter
+import com.kotlindiscord.kord.extensions.commands.converters.Validator
+import com.kotlindiscord.kord.extensions.commands.converters.builders.ConverterBuilder
+import com.kotlindiscord.kord.extensions.commands.converters.builders.OptionalConverterBuilder
+import com.kotlindiscord.kord.extensions.parser.StringParser
+import dev.kord.core.entity.interaction.OptionValue
+import dev.kord.rest.builder.interaction.OptionsBuilder
+import dev.kord.rest.builder.interaction.StringChoiceBuilder
+import nl.pindab0ter.eggbot.model.database.Farmer
+import nl.pindab0ter.eggbot.model.database.Farmers
+import org.jetbrains.exposed.sql.SortOrder.DESC
+import org.jetbrains.exposed.sql.transactions.transaction
+
+/**
+ * Choice converter for database Farmers.
+ */
+class FarmerConverter(
+    override var validator: Validator<Farmer> = null,
+) : ChoiceConverter<Farmer>(choices = transaction {
+    Farmer.all()
+        .limit(25)
+        .orderBy(Farmers.updatedAt to DESC)
+        .associateBy { farmer -> farmer.inGameName }
+}) {
+    override val signatureTypeString: String = "Egg, Inc. Farmer"
+
+    override suspend fun parse(parser: StringParser?, context: CommandContext, named: String?): Boolean {
+        val arg: String = named ?: parser?.parseNext()?.data ?: return false
+
+        parsed = choices.values.firstOrNull { farmer -> farmer.inGameName == arg }
+            ?: throw Exception("Could not get farmer information")
+
+        return true
+    }
+
+    override suspend fun parseOption(context: CommandContext, option: OptionValue<*>): Boolean {
+        parsed = choices.values.firstOrNull { farmer -> farmer.inGameName.trim() == option.value }
+            ?: throw Exception("Could not get farmer information")
+
+        return true
+    }
+
+    override suspend fun toSlashOption(arg: Argument<*>): OptionsBuilder = StringChoiceBuilder(
+        name = arg.displayName,
+        description = arg.description
+    ).apply {
+        required = true
+        this@FarmerConverter.choices.forEach { choice(it.key, it.value.inGameName) }
+    }
+}
+
+class FarmerConverterBuilder : ConverterBuilder<Farmer>() {
+    override fun build(arguments: Arguments): SingleConverter<Farmer> {
+        return arguments.arg(
+            displayName = name,
+            description = description,
+
+            converter = FarmerConverter(
+                validator = validator,
+            ).withBuilder(this)
+        )
+    }
+}
+
+class OptionalFarmerConverterBuilder : OptionalConverterBuilder<Farmer>() {
+    @OptIn(ConverterToOptional::class)
+    override fun build(arguments: Arguments): OptionalConverter<Farmer> {
+        return arguments.arg(
+            displayName = name,
+            description = description,
+
+            converter = FarmerConverter(
+                validator = validator,
+            ).toOptional(
+                outputError = !ignoreErrors,
+                nestedValidator = validator,
+            ).withBuilder(this)
+        )
+    }
+}
+
+fun Arguments.farmer(
+    body: FarmerConverterBuilder.() -> Unit
+): SingleConverter<Farmer> {
+    val builder = FarmerConverterBuilder()
+
+    body(builder)
+
+    builder.validateArgument()
+
+    return builder.build(this)
+}
+
+fun Arguments.optionalFarmer(
+    body: OptionalFarmerConverterBuilder.() -> Unit
+): OptionalConverter<Farmer> {
+    val builder = OptionalFarmerConverterBuilder()
+
+    body(builder)
+
+    builder.validateArgument()
+
+    return builder.build(this)
+}
--- src/main/kotlin/nl/pindab0ter/eggbot/extensions/WhoIsCommand.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/extensions/WhoIsCommand.kt
@@ -1,14 +1,18 @@
 package nl.pindab0ter.eggbot.extensions
 
 import com.kotlindiscord.kord.extensions.commands.Arguments
-import com.kotlindiscord.kord.extensions.commands.converters.impl.user
+import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalUser
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
+import com.kotlindiscord.kord.extensions.utils.suggestStringMap
 import dev.kord.common.annotation.KordPreview
 import dev.kord.core.entity.User
+import nl.pindab0ter.eggbot.converters.optionalFarmer
+import nl.pindab0ter.eggbot.helpers.configuredGuild
 import nl.pindab0ter.eggbot.model.database.Farmer
 import nl.pindab0ter.eggbot.model.database.Farmers
+import org.jetbrains.exposed.sql.SortOrder.DESC
 import org.jetbrains.exposed.sql.transactions.transaction
 
 @KordPreview
@@ -17,9 +21,27 @@ class WhoIsCommand : Extension() {
 
     override suspend fun setup() {
         class WhoIsThisArguments : Arguments() {
-            val discordUser: User by user {
+            val discordUser: User? by optionalUser {
                 name = "member"
                 description = "Find out which farmers this member has registered."
+            }
+            val farmer: Farmer? by optionalFarmer {
+                name = "farmer"
+                description = "Find out which member has registered this farmer."
+
+                autoComplete {
+                    val farmerInput: String = command.options["name"]?.value as String? ?: ""
+
+                    val farmers = transaction {
+                        Farmer
+                            .find { Farmers.inGameName like "%$farmerInput%" }
+                            .orderBy(Farmers.inGameName to DESC)
+                            .limit(25)
+                            .associate { farmer -> farmer.inGameName to farmer.inGameName }
+                    }
+
+                    suggestStringMap(farmers)
+                }
             }
         }
 
@@ -28,20 +50,42 @@ class WhoIsCommand : Extension() {
             description = "Find out who is who."
 
             action {
-                val farmerList = transaction {
-                    val farmers = Farmer.find { Farmers.discordId eq arguments.discordUser.id.toString() }
-                    buildString {
-                        farmers.forEach { farmer ->
-                            when (farmer) {
-                                farmers.first() -> append(farmer.inGameName)
-                                farmers.last() -> append(" and ${farmer.inGameName}")
-                                else -> append(", ${farmer.inGameName}")
+                when {
+                    arguments.discordUser != null && arguments.farmer != null -> {
+                        respond { content = "You can't specify both a member and a farmer." }
+                    }
+
+                    arguments.discordUser != null -> {
+                        val farmerList = transaction {
+                            val farmers = Farmer.find { Farmers.discordId eq arguments.discordUser?.id.toString() }
+                            buildString {
+                                farmers.forEach { farmer ->
+                                    when (farmer) {
+                                        farmers.first() -> append(farmer.inGameName)
+                                        farmers.last() -> append(" and ${farmer.inGameName}")
+                                        else -> append(", ${farmer.inGameName}")
+                                    }
+                                }
                             }
+                        }
+
+                        respond {
+                            content =
+                                if (farmerList.isNotEmpty()) "${arguments.discordUser?.mention} has: $farmerList"
+                                else "${arguments.discordUser?.mention} has not registered any farmers."
+                        }
+                    }
+
+                    arguments.farmer != null -> {
+                        val discordUser = arguments.farmer?.discordId?.let { configuredGuild?.getMember(it) }
+
+                        respond {
+                            content =
+                                if (discordUser != null) "${arguments.farmer?.inGameName} is registered by ${discordUser.mention}"
+                                else "**Error:** Failed to find member for ${arguments.farmer?.inGameName}."
                         }
                     }
                 }
-
-                respond { content = "${arguments.discordUser.mention} has: $farmerList" }
             }
         }
     }
