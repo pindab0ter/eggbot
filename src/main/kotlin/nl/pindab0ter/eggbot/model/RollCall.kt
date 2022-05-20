73271856ab7ba41ae487c9bd3ce5e6fd8606a4b5
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2022-05-20 17:09:08 +0200

    Exclude farmers from inactive Discord users from roll call

--- src/main/kotlin/nl/pindab0ter/eggbot/model/RollCall.kt
+++ src/main/kotlin/nl/pindab0ter/eggbot/model/RollCall.kt
@@ -1,9 +1,12 @@
 package nl.pindab0ter.eggbot.model
 
 import nl.pindab0ter.eggbot.helpers.mapCartesianProducts
+import nl.pindab0ter.eggbot.model.database.DiscordUsers
 import nl.pindab0ter.eggbot.model.database.Farmer
+import nl.pindab0ter.eggbot.model.database.Farmers
 import org.jetbrains.exposed.dao.with
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import kotlin.math.ceil
 import kotlin.math.floor
@@ -19,20 +22,22 @@ fun createRollCall(
     maxCoopSize: Int,
     database: Database?
 ): List<Pair<String, List<Farmer>>> = transaction(database) {
-    val farmers = Farmer.all().with(Farmer::discordUser).toList()
-    val activeFarmers = farmers.filter { it.isActive }.sortedByDescending { it.earningsBonus }
-    val inactiveFarmers = farmers.filter { !it.isActive }.sortedBy { it.earningsBonus }
+    val farmers = Farmer.wrapRows(Farmers.innerJoin(DiscordUsers)
+        .select { DiscordUsers.inactiveUntil.isNull() })
+        .with(Farmer::discordUser)
+        .toList()
+
     val preferredCoopSize: Int =
         if (maxCoopSize <= 10) maxCoopSize
         else (maxCoopSize * FILL_PERCENTAGE).roundToInt()
 
     // Fill each co-op with the strongest available player so that all co-ops have one
     val coops = coopNames((farmers.size / preferredCoopSize) + 1, baseName).mapIndexed { i, coopName ->
-        coopName to mutableListOf(activeFarmers[i])
+        coopName to mutableListOf(farmers[i])
     }
 
     // With the remaining active farmers keep adding the next highest rated to the lowest rated co-op
-    activeFarmers.drop(coops.size).forEach { activeFarmer ->
+    farmers.drop(coops.size).forEach { activeFarmer ->
         coops
             // Filter out co-ops that have enough farmers
             .filter { (_, farmers) -> farmers.count() <= preferredCoopSize }
@@ -44,16 +49,6 @@ fun createRollCall(
             .minByOrNull { (_, farmers) -> farmers.sumOf { it.earningsBonus } }
             // Add the farmer to the co-op
             ?.let { (_, farmers) -> farmers.add(activeFarmer) }
-    }
-
-    // Finally, spread inactive farmers over the coops
-    inactiveFarmers.forEach { inactiveFarmer ->
-        coops
-            // Sort the coops by amount of farmers
-            .sortedBy { (_, farmers) -> farmers.count() }
-            // Get the coop with the least amount of inactive farmers or all
-            .minByOrNull { (_, farmers) -> farmers.count { farmer -> !farmer.isActive } }
-            ?.let { (_, farmers) -> farmers.add(inactiveFarmer) }
     }
 
     coops.map { (name, farmers) -> name to farmers }
