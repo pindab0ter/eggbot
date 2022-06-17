185decca2a0acbff036cff35b8f1ed4a0266c0ce
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2022-06-17 09:44:02 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ application {
 plugins {
     idea
     application
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.serialization") version "1.6.21"
-    id("com.squareup.wire") version "4.3.0"
+    kotlin("jvm") version "1.7.0"
+    kotlin("plugin.serialization") version "1.7.0"
+    id("com.squareup.wire") version "4.4.0"
     id("com.github.ben-manes.versions") version "0.42.0"
 }
 
@@ -32,25 +32,25 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8", "1.6.0")
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.6.1")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.6.2")
     implementation("joda-time", "joda-time", "2.10.14")
     implementation("ch.obermuhlner", "big-math", "2.3.0")
 
 
     // Configuration
-    implementation("com.charleskorn.kaml", "kaml", "0.44.0")
+    implementation("com.charleskorn.kaml", "kaml", "0.45.0")
 
     // Database
     implementation("org.jetbrains.exposed", "exposed-core", "0.38.2")
     implementation("org.jetbrains.exposed", "exposed-dao", "0.38.2")
     implementation("org.jetbrains.exposed", "exposed-jdbc", "0.38.2")
     implementation("org.jetbrains.exposed", "exposed-jodatime", "0.38.2")
-    runtimeOnly("org.postgresql", "postgresql", "42.3.5")
-    implementation("org.flywaydb", "flyway-core", "8.5.11")
+    runtimeOnly("org.postgresql", "postgresql", "42.4.0")
+    implementation("org.flywaydb", "flyway-core", "8.5.12")
 
     // Networking
     implementation("com.github.kittinunf.fuel", "fuel", "2.3.1")
-    implementation("com.squareup.wire", "wire-gradle-plugin", "4.3.0")
+    implementation("com.squareup.wire", "wire-gradle-plugin", "4.4.0")
 
     // Discord
     implementation("com.kotlindiscord.kord.extensions", "kord-extensions", "1.5.3-SNAPSHOT")
@@ -60,8 +60,8 @@ dependencies {
 
     // Logging
     runtimeOnly("ch.qos.logback", "logback-classic", "1.2.11")
-    implementation("io.github.microutils", "kotlin-logging-jvm", "2.1.21")
-    implementation("io.sentry", "sentry", "6.0.0-beta.3")
+    implementation("io.github.microutils", "kotlin-logging-jvm", "2.1.23")
+    implementation("io.sentry", "sentry", "6.1.0")
 }
 
 tasks {
