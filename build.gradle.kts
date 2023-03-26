9a74300f16be4877aa562f09af378779cf8592ad
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2023-03-26 14:49:21 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ application {
 plugins {
     idea
     application
-    kotlin("jvm") version "1.8.0-RC"
-    kotlin("plugin.serialization") version "1.8.0-RC"
-    id("com.squareup.wire") version "4.5.2"
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.10"
+    id("com.squareup.wire") version "4.5.3"
     id("com.github.ben-manes.versions") version "0.46.0"
 }
 
@@ -35,36 +35,36 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8", "1.6.0")
+    implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8", "1.8.10")
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.6.4")
-    implementation("joda-time", "joda-time", "2.12.2")
+    implementation("joda-time", "joda-time", "2.12.4")
     implementation("ch.obermuhlner", "big-math", "2.3.2")
 
     // Configuration
-    implementation("com.charleskorn.kaml", "kaml", "0.52.0")
+    implementation("com.charleskorn.kaml", "kaml", "0.53.0")
 
     // Database
     implementation("org.jetbrains.exposed", "exposed-core", "0.41.1")
     implementation("org.jetbrains.exposed", "exposed-dao", "0.41.1")
     implementation("org.jetbrains.exposed", "exposed-jdbc", "0.41.1")
     implementation("org.jetbrains.exposed", "exposed-jodatime", "0.41.1")
-    implementation("org.flywaydb", "flyway-core", "9.15.2")
-    runtimeOnly("org.postgresql", "postgresql", "42.5.4")
+    implementation("org.flywaydb", "flyway-core", "9.16.1")
+    runtimeOnly("org.postgresql", "postgresql", "42.6.0")
 
     // Networking
     implementation("com.github.kittinunf.fuel", "fuel", "2.3.1")
-    implementation("com.squareup.wire", "wire-gradle-plugin", "4.5.2")
+    implementation("com.squareup.wire", "wire-gradle-plugin", "4.5.3")
 
     // Discord
-    implementation("com.kotlindiscord.kord.extensions", "kord-extensions", "1.5.6-SNAPSHOT")
+    implementation("com.kotlindiscord.kord.extensions", "kord-extensions", "1.5.6")
 
     // Task scheduling
     implementation("org.quartz-scheduler", "quartz", "2.3.2")
 
     // Logging
-    runtimeOnly("ch.qos.logback", "logback-classic", "1.4.5")
+    runtimeOnly("ch.qos.logback", "logback-classic", "1.4.6")
     implementation("io.github.microutils", "kotlin-logging-jvm", "3.0.4")
-    implementation("io.sentry", "sentry", "6.15.0")
+    implementation("io.sentry", "sentry", "6.16.0")
 }
 
 kotlin {
